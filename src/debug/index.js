314327ad20fb88a4df91e2ebf100667e91e43c58
eduardo <soka_117@live.com>
2021-08-14 02:17:00 -0500

    Commit 4 / modificacion a debug/index.js

---
+++ src/debug/index.js
@@ -0,0 +1,27 @@
+const a = 'Hello'
+
+function hello () {
+    let b = 'Hello Word'
+    const c = 'Hello Word!'
+    if (true) {
+        let d = 'Hello Word!!'
+        debugger
+    }
+}
+
+hello()
+
+const moneyBox = () => {
+    var saveCoins = 0
+    const countCoins = (coins) => {
+        saveCoins += coins
+        console.log(`MoneyBox: $${saveCoins}`)
+    }
+    return countCoins
+}
+
+let myMoneyBox = moneyBox()
+
+myMoneyBox(4) 
+myMoneyBox(6) 
+myMoneyBox(10) 
